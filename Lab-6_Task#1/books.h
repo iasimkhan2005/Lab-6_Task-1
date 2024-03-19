24c645e716a91fd7b3854adc2e2891a36db953f3
Asim Khan <146324828+iasimkhan2005@users.noreply.github.com>
2024-03-19 19:57:31 +0500

    Update books.h

--- Lab-6_Task#1/books.h
+++ Lab-6_Task#1/books.h
@@ -1,13 +1,9 @@
 #pragma once
 #include "Library.h"
-class books :
-    public Library
+class books
 {
 	string book_name;
 	int serial_number;
-public:
-	void addBook(books book, books serial_number);
-	void removeBook(books serial_number);
-	void searchBook(books book);
+
 };
 
